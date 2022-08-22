3161f30ae8c14eb082754c90ac376575a7fe1033
Arthur Misko <arthurmisko@gmail.com>
2022-08-22 21:21:30 +0300

    Update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
     const area = document.getElementById('area');
-    let move = 0;
-    let result = '';
     const contentWrapper = document.getElementById('content');
     const modalResult = document.getElementById('modal-result-wrapper');
     const overlay = document.getElementById('overlay');
     const btnClose = document.getElementById('btn-close');
+    let move = 0;
+    let result;
 
     area.addEventListener('click', e => {
         if (e.target.className = 'box') {
@@ -19,32 +19,38 @@
         const arr = [
             [0,1,2],
             [0,4,8],
+            [0,3,6],
             [3,4,5],
             [6,7,8],
-            [0,3,6],
             [1,4,7],
             [2,5,8],
             [2,4,6],
         ];
-        for(i= 0; i < arr.length; i++) {
+        
+        for(let i = 0; i < arr.length; i++) {
             if(
                 boxes[arr[i][0]].innerHTML == 'X' && boxes[arr[i][1]].innerHTML == 'X' && boxes[arr[i][2]].innerHTML == 'X'
             ) {
                 result = 'крестики';
                 prepareResult(result);
+                console.log(result);
             } else if (
                 boxes[arr[i][0]].innerHTML == 'O' && boxes[arr[i][1]].innerHTML == 'O' && boxes[arr[i][2]].innerHTML == 'O'
             ) {
                 result ='нолики';
-                 prepareResult(result);
-            
-            }  
-    }
+                prepareResult(result);
+                console.log(result);
+            } else if (move==9){
+                counter ='..Что? У нас ничья!';
+                prepareResult(counter);
+                console.log(result);
+                console.log(counter);
+            }
+    }  
     }
     
 
     const prepareResult = winner => {
-       console.log(result);
         contentWrapper.innerHTML = `Победили ${winner} !`;
         modalResult.style.display = 'block';
     }
